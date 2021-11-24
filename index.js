f5891ea3213d1d68872e420910d3456215a8eae1
Harshvardhan Sahu <hs7757@srmist.edu.in>
2021-11-24 13:34:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const http = require('http');
+const path = require('path');
+const app = express()
+const server = http.createServer(app);
+var io=require('socket.io')(server);
+
+// app.get('/',(req,res)=>{
+//     res.sendFile(__dirname+'/public/index.html');
+// })
+app.use(express.static(path.join(__dirname,'public')))
+
+io.on('connection',(socket)=>{
+    // console.log('user connected');
+    socket.on('createmsg',(data)=>{
+        io.sockets.emit('newmsg',data);
+    })
+})
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, ()=> console.log(`Server running at port number ${PORT}`))
